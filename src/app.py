6db22f92539ec204d151d7ca76c2519608d53578
soham banerjee <sb2344@njit.edu>
2019-12-20 02:40:36 -0500

    Fix:Edited alembic

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ from flask import Flask
 
 from .config import app_config
 from .models import db, bcrypt
-from .views.UserView import user_api as user_blueprint
+#from .views.UserView import user_api as user_blueprint
 
 
 def create_app(env_name):
@@ -20,7 +20,7 @@ def create_app(env_name):
     bcrypt.init_app(app)
 
     db.init_app(app)
-    app.register_blueprint(user_blueprint, url_prefix='/api/v1/users')
+    #app.register_blueprint(user_blueprint, url_prefix='/api/v1/users')
 
     @app.route('/', methods=['GET'])
     def index():
