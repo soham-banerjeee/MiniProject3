3a9947a4c3564c89033df99117ea0c4b44688222
soham banerjee <sb2344@njit.edu>
2019-12-20 01:12:32 -0500

    Task:Updated BlogpostModel.py

--- src/models/BlogpostModel.py
+++ src/models/BlogpostModel.py
@@ -1,7 +1,7 @@
 # src/models/BlogpostModel.py
 from . import db
 import datetime
-
+from marshmallow import fields, Schema
 
 class BlogpostModel(db.Model):
     """
@@ -13,12 +13,16 @@ class BlogpostModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(128), nullable=False)
     contents = db.Column(db.Text, nullable=False)
+    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     created_at = db.Column(db.DateTime)
     modified_at = db.Column(db.DateTime)
 
     def __init__(self, data):
         self.title = data.get('title')
         self.contents = data.get('contents')
+        self.created_at = datetime.datetime.utcnow()
+        self.modified_at = datetime.datetime.utcnow()
+        self.owner_id = data.get('owner_id')
         self.created_at = datetime.datetime.utcnow()
         self.modified_at = datetime.datetime.utcnow()
 
@@ -46,3 +50,14 @@ class BlogpostModel(db.Model):
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
+
+class BlogpostSchema(Schema):
+  """
+  Blogpost Schema
+  """
+  id = fields.Int(dump_only=True)
+  title = fields.Str(required=True)
+  contents = fields.Str(required=True)
+  owner_id = fields.Int(required=True)
+  created_at = fields.DateTime(dump_only=True)
+  modified_at = fields.DateTime(dump_only=True)
